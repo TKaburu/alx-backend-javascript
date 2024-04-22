d2311f012073841f0b6ebc122b1789832afb1f2b
TKaburu <traceymwendwa@gmail.com>
2024-04-22 19:51:00 +0300

    Feat: Block Scope

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,10 @@
+export default function taskBlock(trueOrFalse) {
+    var task = false;
+    var task2 = true;
+  
+    if (trueOrFalse) {
+        // pass
+    }
+  
+    return [task, task2];
+  }
