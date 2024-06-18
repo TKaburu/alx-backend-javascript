24f83d799e9b5ec1013a013c3528816412b83974
TKaburu <traceymwendwa@gmail.com>
2024-06-18 23:59:25 +0300

    fix: output

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,10 +1,10 @@
-const { createServer } = require('node:http'); // http module
+const http = require('node:http'); // http module
 
 // set hostname and port
 const hostname = '127.0.0.1';
 const port = 1245;
 
-const app = createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200; // ok/successful status
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
