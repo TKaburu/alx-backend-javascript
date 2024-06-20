7ce4b191b3ea58ac87e17aee4d12c9d3a104ac77
TKaburu <traceymwendwa@gmail.com>
2024-06-21 01:14:15 +0300

    feat: Regex integration testing

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,40 @@
+const chai = require('chai');
+const request = require('request');
+
+describe('Index page', () => {
+  describe('GET /', () => {
+    it('should test for  GET / request', (done) => {
+      request('http://localhost:7865', (error, res, body) => {
+        chai.expect(res.statusCode).to.equal(200);
+        chai.expect(body).to.equal('Welcome to the payment system');
+        done();
+      });
+    });
+
+  describe('cart functionality', () => {
+    it('should return status code 200 when id is a number', (done) => {
+      request('http://localhost:7865/cart/55', (error, res, body) => {
+        chai.expect(res.statusCode).to.equal(200);
+        chai.expect(body).to.equal('Payment methods for cart 55');
+        done();
+      });
+    });
+
+    it('should return status code 404 when id is not a number', (done) => {
+      request('http://localhost:7865/cart/tkcodes', (error, res, body) => {
+        chai.expect(res.statusCode).to.equal(404);
+        chai.expect(res.statusCode).to.equal(404);
+        done();
+      });
+    });
+  });
+
+  describe('GET /cart/:id', () => {
+    
+  });
+
+  
+    
+
+  });
+});
